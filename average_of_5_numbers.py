238e5ace3005c22e3e91e418d4739db0e8cb6509
Sumit Tech <161143974+Sumit3754@users.noreply.github.com>
2025-08-28 22:06:49 +0530

    Delete average_of_5_numbers.py

--- average_of_5_numbers.py
+++
@@ -1,8 +0,0 @@
-A = int(input("Enter number A: "))
-B = int(input("Enter number B: "))
-C = int(input("Enter number C: "))
-D = int(input("Enter number D: "))
-E = int(input("Enter number E: "))
-
-avg = (A + B + C + D + E) / 5
-print("Average is:", avg)
